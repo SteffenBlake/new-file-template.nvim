d7fddecf96c49402e09d2bfaf597a233189663aa
steffen.cole.blake@gmail.com <steffen.cole.blake@gmail.com>
2024-01-24 17:44:01 -0700

    Lua template template spacing

--- lua/new-file-template/templates/lua.lua
+++ lua/new-file-template/templates/lua.lua
@@ -6,9 +6,10 @@ local function new_file_template(_, _, callback)
 local utils = require("new-file-template.utils")
 
 local function base_template(relative_path, filename, callback)
-  callback([[
-|cursor|
-  ]])
+
+    callback([[
+        |cursor|
+    ]])
 end
 
 --- @param opts table
@@ -18,11 +19,11 @@ end
 ---   - `filename` (string): The filename of the new file, e.g., "init.lua".
 ---   - `disable_specific` (table): An array of patterns that should be disabled, e.g., { "lua/templates/.*" }.
 return function(opts, callback)
-  local template = {
-    { pattern = ".*", content = base_template },
-  }
+    local template = {
+        { pattern = ".*", content = base_template },
+    }
 
-  return utils.find_entry(template, opts, callback)
+    return utils.find_entry(template, opts, callback)
 end]=])
 end
 
@@ -32,9 +33,9 @@ local function new_file_template_for_user(_, _, callback)
 local utils = require("new-file-template.utils")
 
 local function base_template(relative_path, filename, callback)
-  callback([[
+    callback([[
 |cursor|
-  ]])
+    ]])
 end
 
 --- @param opts table
@@ -43,9 +44,9 @@ end
 ---   - `relative_path` (string): The relative path of the new file, e.g., "lua/new-file-template/templates/init.lua".
 ---   - `filename` (string): The filename of the new file, e.g., "init.lua".
 return function(opts, callback)
-  local template = {
-    { pattern = ".*", content = base_template },
-  }
+    local template = {
+        { pattern = ".*", content = base_template },
+    }
 
 	return utils.find_entry(template, opts, callback)
 end]=])
@@ -66,7 +67,7 @@ end
 local function new_plugin(_, _, callback)
 	callback([[
 return {
-  |cursor|
+    |cursor|
 }]])
 end
 
